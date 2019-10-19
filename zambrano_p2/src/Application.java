3841163db055a4ffac692a50a6f998d87ed3ce3a
masonzambrano <masonzambrano@MacBook-Pro.local>
2019-10-19 12:25:43 -0400

    Second commit

--- zambrano_p2/src/Application.java
+++ zambrano_p2/src/Application.java
@@ -3,12 +3,17 @@ public class Application {
         SavingsAccount saver1 = new SavingsAccount();
         SavingsAccount saver2 = new SavingsAccount();
 
-        for(int i = 0; i < 12; i++) {
-            System.out.print(saver1.calculateMonthlyInterest(3000.00, .04) + "\t");
-            System.out.print(saver2.calculateMonthlyInterest(4000.00, .04) + "\n");
+        double savings1 = 3000.00;
+        double savings2 = 4000.00;
+
+        for(int i = 1; i < 13; i++) {
+            System.out.printf("Month %d: %.2f\t", i, saver1.calculateMonthlyInterest(savings1, .04));
+            savings1 = savings1 + saver1.calculateMonthlyInterest(savings1, .04);
+            System.out.printf("%.2f\n", saver2.calculateMonthlyInterest(savings2, .04));
+            savings2 = savings2 + saver2.calculateMonthlyInterest(savings2, .04);
         }
 
-        System.out.print(saver1.calculateMonthlyInterest(3000.00,  .05)+ "\t");
-        System.out.print(saver1.calculateMonthlyInterest(4000.00, .05));
+        System.out.printf("Month 13: %.2f\t", saver1.calculateMonthlyInterest(savings1,  .05));
+        System.out.printf("%.2f", saver1.calculateMonthlyInterest(savings2, .05));
     }
 }
